ae96eead901d7946f44618c096393619844d5a68
mrasynchronous <itsmrasync@gmail.com>
2024-02-08 14:03:36 -0800

    initial commit

---
+++ src/panels/HelloWorldPanel.ts
@@ -0,0 +1,36 @@
+import * as vscode from "vscode";
+
+export class HelloWorldPanel {
+  public static currentPanel: HelloWorldPanel | undefined;
+  private readonly _panel: vscode.WebviewPanel;
+  private _disposables: vscode.Disposable[] = [];
+
+  private constructor(panel: vscode.WebviewPanel) {
+    this._panel = panel;
+  }
+
+  public static render() {
+    if (HelloWorldPanel.currentPanel) {
+      HelloWorldPanel.currentPanel._panel.reveal(vscode.ViewColumn.One);
+    } else {
+      const panel = vscode.window.createWebviewPanel("hello-world", "Hello World", vscode.ViewColumn.One, {
+        // Empty for now
+      });
+
+      HelloWorldPanel.currentPanel = new HelloWorldPanel(panel);
+    }
+  }
+
+  public dispose() {
+    HelloWorldPanel.currentPanel = undefined;
+
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+}
